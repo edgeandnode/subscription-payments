e71b4e6da83d8f5220862f00619b6be20bb60cfd
Tomás Migone <tomas@edgeandnode.com>
2023-02-06 15:14:00 -0300

    Update contract/contracts/Subscriptions.sol
    Co-authored-by: Theo Butler <theodusbutler@gmail.com>

--- contract/contracts/Subscriptions.sol
+++ contract/contracts/Subscriptions.sol
@@ -73,7 +73,7 @@ contract Subscriptions {
     // Convert block number to epoch number, rounding up to the next epoch
     // boundry.
     function blockToEpoch(uint256 b) public view returns (uint256) {
-        return Math.max(1, (b / epochBlocks) + Math.min(1, (b % epochBlocks) + 1));
+        return (b / epochBlocks) + 1;
     }
 
     /**
