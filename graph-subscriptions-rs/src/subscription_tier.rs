9bb1896305f3e70af25a6da84bdead21a27a1509
Chris Whited <cmwhited1990@gmail.com>
2023-04-19 06:11:10 -1000

    feat(subscription tiers): add subscription tier holder structs (#50)
    * feat(subscription tiers): add subscription tier holder structs
    
    * feat(subscription tiers): PR feedback

---
+++ graph-subscriptions-rs/src/subscription_tier.rs
@@ -0,0 +1,29 @@
+use serde::Deserialize;
+use serde_with::{serde_as, DisplayFromStr};
+
+#[derive(Debug, Default)]
+pub struct SubscriptionTiers(Vec<SubscriptionTier>);
+
+#[serde_as]
+#[derive(Clone, Debug, Default, Deserialize)]
+pub struct SubscriptionTier {
+    /// Payment rate from the subcription contract.
+    #[serde_as(as = "DisplayFromStr")]
+    pub payment_rate: u128,
+    /// Maximum query rate allowed, in queries per second.
+    pub query_rate_limit: u32,
+}
+
+impl SubscriptionTiers {
+    pub fn new(mut tiers: Vec<SubscriptionTier>) -> Self {
+        tiers.sort_by_key(|t| t.payment_rate);
+        Self(tiers)
+    }
+    pub fn tier_for_rate(&self, sub_rate: u128) -> SubscriptionTier {
+        self.0
+            .iter()
+            .find(|tier| tier.payment_rate <= sub_rate)
+            .cloned()
+            .unwrap_or_default()
+    }
+}
