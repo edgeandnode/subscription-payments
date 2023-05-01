62068e09c07960ec2e266da8719ab9437a36eabf
Chris Whited <cmwhited1990@gmail.com>
2023-05-01 10:03:07 -1000

    feat(sub api | k8s): remove local k8s configs. moving to using graph-infra

--- graph-subscriptions-api/.k8s/production/auth.sh
+++
@@ -1,1 +0,0 @@
-gcloud container clusters get-credentials hosted-service --project the-graph-production --zone us-central1-a
--- graph-subscriptions-api/.k8s/production/update-env.sh
+++
@@ -1,3 +0,0 @@
-./auth.sh
-kubectl apply -f env.yml
-kubectl rollout restart deploy/graph-subscriptions-api
